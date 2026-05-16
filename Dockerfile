e9b97df5a2f6b3147db86580e9902a4980252346
BrodyZhang <296068994@qq.com>
2026-05-16 20:43:15 +0800

    Simplify entrypoint for cert generation

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM nginx:alpine
 
-RUN apk add --no-cache openssl
-
 COPY src/index.html /usr/share/nginx/html/
 COPY nginx.conf /etc/nginx/conf.d/default.conf
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,29 @@
 #!/bin/sh
 
 if [ -f "/etc/letsencrypt/live/ailaopo.online/fullchain.pem" ]; then
-    exec nginx -g "daemon off;"
-else
-    # Generate self-signed cert for initial startup
-    mkdir -p /etc/nginx/ssl
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-        -keyout /etc/nginx/ssl/server.key \
-        -out /etc/nginx/ssl/server.crt \
-        -subj "/C=CN/ST=Beijing/L=Beijing/O=MySite/CN=localhost"
+    cat > /etc/nginx/conf.d/default.conf << 'EOF'
+server {
+    listen 80;
+    server_name ailaopo.online www.ailaopo.online;
+    return 301 https://$host$request_uri;
+}
 
-    # Use non-SSL config
-    sed -i 's/listen 443 ssl;/listen 443;/' /etc/nginx/conf.d/default.conf
-    sed -i '/ssl_certificate/d' /etc/nginx/conf.d/default.conf
-    exec nginx -g "daemon off;"
+server {
+    listen 443 ssl;
+    server_name ailaopo.online www.ailaopo.online;
+
+    ssl_certificate /etc/letsencrypt/live/ailaopo.online/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/ailaopo.online/privkey.pem;
+
+    ssl_protocols TLSv1.2 TLSv1.3;
+    ssl_prefer_server_ciphers off;
+
+    location / {
+        root /usr/share/nginx/html;
+        index index.html;
+    }
+}
+EOF
 fi
+
+exec nginx -g "daemon off;"
